8bad24fdf18921e24e48983e7165d0e0dc37cad4
JOASH BRAHMA <166527376+JOASH-JSH@users.noreply.github.com>
2024-08-01 22:40:36 +0530

    Add class TaskItem

---
+++ src/app/data/taskItem.js
@@ -0,0 +1,10 @@
+"use strict";
+
+export default class TaskItem {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
