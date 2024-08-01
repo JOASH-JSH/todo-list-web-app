5eb98ea83469616882d6443140201f5bafb94f34
JOASH BRAHMA <166527376+JOASH-JSH@users.noreply.github.com>
2024-08-01 21:58:05 +0530

    Add initial files

---
+++ webpack.dev.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
