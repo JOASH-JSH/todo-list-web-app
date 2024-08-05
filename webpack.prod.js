3362eebff3dc8221ac265a8a63e90e47a0f79823
JOASH BRAHMA <166527376+JOASH-JSH@users.noreply.github.com>
2024-08-05 13:20:14 +0530

    Modified

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,46 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        filename: "[name].[contenthash].js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.[contenthash].html",
+        }),
+        new MiniCssExtractPlugin({ filename: "index.[contenthash].css" }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(svg|ico)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "./assets/icons/[name].[hash][ext]",
+                },
+            },
+            {
+                test: /\.(webp|png)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "./assets/images/[name].[hash][ext]",
+                },
+            },
+        ],
+    },
 };
